821fcb6494fcc5ac1eb18ecbbeca36649ec947f0
SPoluliakh <S_Poluliakh@ukr.net>
2022-12-15 22:48:08 +0200

    fixed

--- src/components/StatisticSection/StatisticSection.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const StatText = styled.h3`
-  display: flex;
-  justify-content: center;
-  font-weight: ${p => p.theme.fontWeights.normal};
-  font-size: ${p => p.theme.fontSizes.ml}px;
-`;
